0031733668445e5096760867a5a827acec6e9814
Z User <z@container>
2026-06-06 05:12:09 +0000

    dcc967c6-9b51-417b-a26c-307af8432878

--- .zscripts/dev.sh
+++ .zscripts/dev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/z/my-project
+# Ensure static files are in place
 cp -r .next/static .next/standalone/.next/ 2>/dev/null
 cp -r public .next/standalone/ 2>/dev/null
+# Start the standalone production server
 cd .next/standalone
-node server.js
+exec node server.js
---
+++ start-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/z/my-project/.next/standalone
+exec node server.js
